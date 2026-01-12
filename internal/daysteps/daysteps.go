0677829b696aa144a45c1c36bc2b9297e6c1de77
Alexandr Belousov <alxbel94@gmail.com>
2026-01-13 00:20:38 +0300

    Fix tests

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -3,6 +3,7 @@ package daysteps
 import (
 	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 	"time"
@@ -17,8 +18,8 @@ const (
 	mInKm = 1000
 )
 
-func parsePackage(data string) (int, time.Duration, error) { // "678,0h50m"
-	refactoredData := strings.Split(data, ",") // ['678' '0h50m']
+func parsePackage(data string) (int, time.Duration, error) {
+	refactoredData := strings.Split(data, ",")
 	if len(refactoredData) == 2 {
 		steps, err := strconv.Atoi(refactoredData[0])
 		if err != nil || steps <= 0 {
@@ -26,17 +27,25 @@ func parsePackage(data string) (int, time.Duration, error) { // "678,0h50m"
 				err = errors.New("неверные шаги")
 			}
 			fmt.Println("Ошибка:", err)
+			log.Println(err)
 			return 0, 0, err
 		}
 		duration, err := time.ParseDuration(refactoredData[1])
-		if err != nil {
+		if err != nil || duration <= 0 {
+			if err == nil {
+				err = errors.New("неверная продолжительность")
+			}
 			fmt.Println("Ошибка:", err)
+			log.Println(err)
 			return 0, 0, err
 		}
 
 		return steps, duration, nil
 	}
-	return 0, 0, errors.New("Количество элементов в слайсе не равно 2")
+	err := errors.New("количество элементов в слайсе не равно 2")
+	fmt.Println("Ошибка:", err)
+	log.Println(err)
+	return 0, 0, err
 }
 
 func DayActionInfo(data string, weight, height float64) string {
@@ -52,9 +61,9 @@ func DayActionInfo(data string, weight, height float64) string {
 		return ""
 	}
 	return fmt.Sprintf(
-		"Количество шагов: %d\n"+
-			"Дистанция составила: %.2f км.\n"+
-			"Вы сожгли: %.2f",
+		"Количество шагов: %d.\n"+
+			"Дистанция составила %.2f км.\n"+
+			"Вы сожгли %.2f ккал.\n",
 		steps,
 		distance,
 		calories,
